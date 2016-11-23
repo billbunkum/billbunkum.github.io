6f4ef92a896d7e19a8b2e2ca7dae997127db5438
billbunkum <william.richard.hurst@gmail.com>
2016-11-23 16:31:33 -0500

    add functionality to dropdown on index page

--- app.js
+++ app.js
@@ -1,8 +1,54 @@
 angular.module('root', [])
     .controller('RootCtrl', RootController);
 
+// currently NOT DRY
+
 function RootController() {
     const ctrl = this;
-    ctrl.datebyhate = false;
+
+// controller scalars
+    ctrl.details = false;
+    ctrl.barkinapp = false;
     ctrl.dotheangle = false;
+    ctrl.grocerylist = false;
+    ctrl.datebyhate = false;
+
+// controller functions
+    function getDetailsBarkinApp() {
+    ctrl.details = true;
+    ctrl.barkinapp = true;
+    ctrl.dotheangle = false;
+    ctrl.grocerylist = false;
+    ctrl.datebyhate = false;
+    }
+
+    function getDetailsDoTheAngle() {
+    ctrl.details = true;
+    ctrl.barkinapp = false;
+    ctrl.dotheangle = true;
+    ctrl.grocerylist = false;
+    ctrl.datebyhate = false;
+    }
+
+    function getDetailsGroceryList() {
+    ctrl.details = true;
+    ctrl.barkinapp = false;
+    ctrl.dotheangle = false;
+    ctrl.grocerylist = true;
+    ctrl.datebyhate = false;
+    }
+
+    function getDetailsDateByHate() {
+    ctrl.details = true;
+    ctrl.barkinapp = false;
+    ctrl.dotheangle = false;
+    ctrl.grocerylist = false;
+    ctrl.datebyhate = true;
+    }
+
+// defined controller functions
+    ctrl.getDetailsBarkinApp = getDetailsBarkinApp;
+    ctrl.getDetailsDoTheAngle = getDetailsDoTheAngle;
+    ctrl.getDetailsGroceryList = getDetailsGroceryList;
+    ctrl.getDetailsDateByHate = getDetailsDateByHate;
 }
